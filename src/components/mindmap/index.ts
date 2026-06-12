290a17fd8ed776aa43197e8ba4f560c4696027d8
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2026-06-12 12:59:49 -0300

    feat(mindmap): export new modals

--- src/components/mindmap/index.ts
+++ src/components/mindmap/index.ts
@@ -1,3 +1,6 @@
 export { MindMapNode } from "./MindMapNode";
 export { MindMapCanvas } from "./MindMapCanvas";
+export { AddNodeModal } from "./AddNodeModal";
+export { AddTaskModal } from "./AddTaskModal";
+
 export type { MindMapNode as MindMapNodeType, MegaProject, Task } from "@/types/mindmap";
